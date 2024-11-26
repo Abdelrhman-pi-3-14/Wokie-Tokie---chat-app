631cf4314fa15c21a3cb94540cd0d26a1d81683c
Ab_aboelhassan <alainx796@gmail.com>
2024-11-26 03:20:11 +0200

    cleaning the files

--- app/src/main/java/com/example/walkie_talkie/system/data/app_design/feature_ai_chat/repository/ChatPrompt.kt
+++
@@ -1,2 +0,0 @@
-package com.example.walkie_talkie.system.data.app_design.feature_ai_chat.repository
-
--- app/src/main/java/com/example/walkie_talkie/system/data/app_design/feature_ai_chat/repository/Connection.kt
+++ app/src/main/java/com/example/walkie_talkie/system/data/app_design/feature_ai_chat/repository/Connection.kt
@@ -26,65 +26,6 @@ interface NetworkStateProvider {
 class Connection(context: Context) : NetworkStateProvider {
     private var connectionManager =
         context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    /*
-        fun checkConnection() {
-            val network = connectionManager.activeNetwork
-            if (network == null) {
-                postValue(false)
-            }
-            val requestBuilder = NetworkRequest.Builder().apply {
-                addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-                addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-                addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
-                addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-                addTransportType(NetworkCapabilities.TRANSPORT_ETHERNET)
-            }.build()
-            connectionManager.registerNetworkCallback(requestBuilder , networkCallback)
-        }
-
-
-        override fun onActive() {
-            super.onActive()
-            checkConnection()
-        }
-
-        override fun onInactive() {
-            super.onInactive()
-            connectionManager.unregisterNetworkCallback(networkCallback)
-        }
-
-
-
-
-        private val networkCallback = object : android.net.ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network) {
-                super.onAvailable(network)
-                val capabilities = connectionManager.getNetworkCapabilities(network)
-                if (capabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true) {
-
-                }
-            }
-
-            override fun onCapabilitiesChanged(
-                network: Network ,
-                networkCapabilities: NetworkCapabilities
-            ) {
-                super.onCapabilitiesChanged(network , networkCapabilities)
-            }
-
-            override fun onUnavailable() {
-                super.onUnavailable()
-
-            }
-
-
-            override fun onLost(network: Network) {
-                super.onLost(network)
-
-            }
-
-        }
-    */
 
     override fun observe(): Flow<NetworkState> {
         return callbackFlow {
